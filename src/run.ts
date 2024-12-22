e13ea4c9c0bd90a460d47ad6aa54b14fa33b217a
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-22 18:09:09 +0100

    chore: udpate test.yml

--- src/run.ts
+++ src/run.ts
@@ -1,5 +1,39 @@
 #!/usr/bin/env node
+import { join, resolve } from 'path';
+import { execSync } from 'child_process';
+import { EOL } from 'os';
+
+const SEVEN_DIR = resolve(__dirname, '..', '7zip');
+
+function exec(command: string): void {
+  console.log(
+    execSync(command + ' --help')
+      .toString()
+      .split(EOL)
+      .map(l => l.trim())
+      .filter(l => l.length)[0]
+  );
+}
 
 (() => {
-  console.log(process.platform, process.arch);
+  const { platform, arch } = process;
+
+  switch (platform) {
+    case 'win32': {
+      exec(join(SEVEN_DIR, 'win32_x64_7z.exe'));
+      break;
+    }
+
+    case 'darwin': {
+      exec(join(SEVEN_DIR, 'darwin_7z'));
+      break;
+    }
+
+    case 'linux': {
+      exec(join(SEVEN_DIR, 'linux_x64_7z'));
+      break;
+    }
+  }
+
+  console.log(platform, arch);
 })();
