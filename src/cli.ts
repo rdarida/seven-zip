d7acf5eb14c3191995dc13e5489742ab78e5e301
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-23 11:39:59 +0100

    feat: add examples to cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -22,6 +22,10 @@ yargs
     'Creates a compressed archive, or zipped file, from specified files and folders.',
     yargs => {
       return yargs
+        .example(
+          '$0 zip dest.7z file1 file2.txt folder',
+          'Creates a new 7z archive named "dest.7z" containing "file1", "file2.txt", and the contents of the "folder" directory.'
+        )
         .positional('destination', {
           demandOption: true,
           describe: 'Specifies the path to the archive output file',
@@ -48,6 +52,10 @@ yargs
     'Extracts files from a specified archive (zipped) file.',
     yargs => {
       return yargs
+        .example(
+          '$0 unzip source.7z destination',
+          'Extracts the contents of the "source.7z" archive into the "destination" directory. If the folder does not exist, it will be created.'
+        )
         .positional('source', {
           demandOption: true,
           describe: 'Specifies the path to the archive file',
