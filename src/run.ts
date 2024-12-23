c5d21bb0f5af1f838f8a558e4db180f8adee6cc1
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-23 12:13:28 +0100

    feat: test windows and linux x86 binaries on x64 arch

--- src/run.ts
+++ src/run.ts
@@ -22,7 +22,7 @@ function exec(command: string): void {
 
   switch (platform) {
     case 'win32': {
-      exec(join(SEVEN_DIR, 'win32_7z.exe'));
+      exec(join(SEVEN_DIR, 'win32_ia32_7z.exe'));
       break;
     }
 
@@ -32,7 +32,7 @@ function exec(command: string): void {
     }
 
     case 'linux': {
-      exec(join(SEVEN_DIR, 'linux_x64_7z'));
+      exec(join(SEVEN_DIR, 'linux_ia32_7z'));
       break;
     }
   }
