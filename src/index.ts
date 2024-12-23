48862a8f138d02a2c9e514c7855a4e17ad6ee90e
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-23 21:43:34 +0100

    feat: implement get7zipPath function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+import { join, resolve } from 'path';
+import { existsSync } from 'fs';
+
+const ZIP_DIR = resolve(__dirname, '..', '7zip');
+
+function get7zipPath(): string | undefined {
+  const { platform, arch } = process;
+  const ext = platform === 'win32' ? '.exe' : '';
+  let path = join(ZIP_DIR, `${platform}_${arch}_7z` + ext);
+
+  if (existsSync(path)) {
+    return path;
+  }
+
+  path = resolve(ZIP_DIR, `${platform}_7z` + ext);
+
+  if (existsSync(path)) {
+    return path;
+  }
+
+  return undefined;
+}
+
 export function sevenZipSync(paths: string[], destination: string): void {
   console.log('sevenZipSync');
   console.log('paths:', paths);
