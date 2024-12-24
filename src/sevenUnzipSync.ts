3a76c6798ac13ba0bbf202ecd8e8bb2f307963da
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-24 20:47:58 +0100

    style: format comments

--- src/sevenUnzipSync.ts
+++ src/sevenUnzipSync.ts
@@ -5,7 +5,9 @@ import { getSevenZipPath } from './getSevenZipPath';
  * Extracts files from a specified zipped file **synchronously**.
  *
  * @param archive Specifies the path to the zipped file.
+ *
  * @param destination Specifies the path to the output directory.
+ *
  * @throws {Error} Will throw an error if the 7-Zip executable is not found.
  */
 export function sevenUnzipSync(archive: string, destination: string): void {
