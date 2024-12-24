40f7b7fb012371a9f37d6d0684cba177979b8bd1
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-24 14:49:04 +0100

    chore: write documentation for getSevenZipPath function

--- src/getSevenZipPath.ts
+++ src/getSevenZipPath.ts
@@ -3,6 +3,19 @@ import { existsSync } from 'fs';
 
 const ZIP_DIR = resolve(__dirname, '..', '7zip');
 
+/**
+ * Resolves the path to the appropriate 7-Zip executable based on the given
+ * platform and architecture.
+ *
+ * @param platform The platform for which the 7-Zip executable is required.
+ * Defaults to the current platform (`process.platform`) if not provided.
+ *
+ * @param arch The architecture for which the 7-Zip executable is required.
+ * Defaults to the current architecture (`process.arch`) if not provided.
+ *
+ * @returns The resolved path to the 7-Zip executable, or `undefined` if no
+ * matching executable is found.
+ */
 export function getSevenZipPath(
   platform?: typeof process.platform,
   arch?: typeof process.arch
