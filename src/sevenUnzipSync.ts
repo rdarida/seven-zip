88712b184376b49f90c0e6db63af60fca305254b
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-24 15:02:39 +0100

    chore: add documentation for sevenUnzipSync and sevenZipSync functions

--- src/sevenUnzipSync.ts
+++ src/sevenUnzipSync.ts
@@ -1,11 +1,20 @@
 import { executeSync } from './utils';
 import { getSevenZipPath } from './getSevenZipPath';
 
+/**
+ * Extracts files from a specified archive (zipped) file synchronously.
+ *
+ * @param source Specifies the path to the archive file.
+ *
+ * @param destination Specifies the path to the output folder.
+ *
+ * @throws {Error} Will throw an error if the 7-Zip executable is not found.
+ */
 export function sevenUnzipSync(source: string, destination: string): void {
   const command = getSevenZipPath();
 
   if (!command) {
-    throw new Error('There is no command.');
+    throw new Error('7-Zip executable not found.');
   }
 
   const args = ['x', source, `-o${destination}`];
