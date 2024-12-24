409643cd2feee9c3d75521c662635c3f27d5149f
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-24 12:38:26 +0100

    refactor: separate sevenZipSync function from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 export * from './getSevenZipPath';
-
-export function sevenZipSync(paths: string[], destination: string): void {
-  console.log('sevenZipSync');
-  console.log('paths:', paths);
-  console.log('destination:', destination);
-}
-
 export * from './sevenUnzipSync';
+export * from './sevenZipSync';
---
+++ src/sevenZipSync.ts
@@ -0,0 +1,5 @@
+export function sevenZipSync(paths: string[], destination: string): void {
+  console.log('sevenZipSync');
+  console.log('paths:', paths);
+  console.log('destination:', destination);
+}
